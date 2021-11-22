221d8012b9c2f1f067cad235d2f493dd1cbea4dd
zeyu <xanthus@hotmail.sg>
2021-11-22 12:50:24 +0800

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,13 +9,25 @@ const App = () => {
   //When we want to update word, we need to pass new value to setWord function to update
   //2ns element, type function, used to update the value of variable word
   const [word, setWord] = useState(""); //'' initial value
+  const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
+  //Called in real time when search button is pressed
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target[0].value);
+    console.log(e.target[0].value); //word
+    fetch(
+      `https://api.unsplash.com//photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  console.log(word);
+  //  console.log(word);
 
   return (
     //title, word etc are properties of the respective components(Header,Search)
